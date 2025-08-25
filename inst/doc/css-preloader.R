3e80408ff68933d81f0131f67c1f54ae6133a92c
David Granjon <dgranjon@ymail.com>
2025-08-25 12:40:07 +0000

    version 2.0.6

--- inst/doc/css-preloader.R
+++ inst/doc/css-preloader.R
@@ -22,25 +22,25 @@ card(
 )
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  library(shiny)
-#  library(shinydashboard)
-#  library(shinydashboardPlus)
-#  library(waiter)
-#  shinyApp(
-#    ui = dashboardPage(
-#      preloader = list(html = tagList(spin_1(), "Loading ..."), color = "#3c8dbc"),
-#      header = dashboardHeader(),
-#      sidebar = dashboardSidebar(),
-#      body = dashboardBody(
-#        actionButton("reload", "Reload")
-#      ),
-#      title = "Preloader"
-#    ),
-#    server = function(input, output, session) {
-#      # fake reload
-#      observeEvent(input$reload, {
-#        session$reload()
-#      })
-#    }
-#  )
+# library(shiny)
+# library(shinydashboard)
+# library(shinydashboardPlus)
+# library(waiter)
+# shinyApp(
+#   ui = dashboardPage(
+#     preloader = list(html = tagList(spin_1(), "Loading ..."), color = "#3c8dbc"),
+#     header = dashboardHeader(),
+#     sidebar = dashboardSidebar(),
+#     body = dashboardBody(
+#       actionButton("reload", "Reload")
+#     ),
+#     title = "Preloader"
+#   ),
+#   server = function(input, output, session) {
+#     # fake reload
+#     observeEvent(input$reload, {
+#       session$reload()
+#     })
+#   }
+# )
 
--- inst/doc/shinydashboardPlus.R
+++ inst/doc/shinydashboardPlus.R
@@ -22,20 +22,20 @@ card(
 )
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  library(shiny)
-#  library(shinydashboard)
-#  library(shinydashboardPlus)
-#  shinyApp(
-#     ui = dashboardPage(
-#       options = list(sidebarExpandOnHover = TRUE),
-#       header = dashboardHeader(),
-#       sidebar = dashboardSidebar(minified = TRUE, collapsed = TRUE),
-#       body = dashboardBody(
-#        lapply(1:20, box, width = 12, title = "box")
-#       ),
-#       controlbar = dashboardControlbar(),
-#       title = "DashboardPage"
-#     ),
-#     server = function(input, output) { }
-#   )
+# library(shiny)
+# library(shinydashboard)
+# library(shinydashboardPlus)
+# shinyApp(
+#    ui = dashboardPage(
+#      options = list(sidebarExpandOnHover = TRUE),
+#      header = dashboardHeader(),
+#      sidebar = dashboardSidebar(minified = TRUE, collapsed = TRUE),
+#      body = dashboardBody(
+#       lapply(1:20, box, width = 12, title = "box")
+#      ),
+#      controlbar = dashboardControlbar(),
+#      title = "DashboardPage"
+#    ),
+#    server = function(input, output) { }
+#  )
 
